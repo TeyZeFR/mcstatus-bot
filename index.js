b6c39043fed71659ccdbcd8d7b03587fc1daaa47
TeyZeFR <anthony70400@gmail.com>
2025-05-14 18:35:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is running');
+}).listen(process.env.PORT || 3000);
+
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const fetch = require('node-fetch');
